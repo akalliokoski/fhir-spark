6d9bea8fb82cd6cbd47564dc345c0f82da69137c
Antti Kalliokoski <antti.a.kalliokoski@gmail.com>
2018-10-05 11:14:14 +0300

    Fix serialization error by changing the map function to a variable.
    For details, see https://stackoverflow.com/questions/40818001/understanding-spark-serialization

--- src/main/scala/BunsenTest.scala
+++ src/main/scala/BunsenTest.scala
@@ -2,17 +2,27 @@ package fi.codeo
 
 import ca.uhn.fhir.context.FhirContext
 import com.cerner.bunsen.FhirEncoders
-import org.apache.spark._
-import org.apache.spark.sql.{Dataset, Encoders, Row, SparkSession}
+import org.apache.spark.sql.{SparkSession}
 import org.apache.log4j._
 import org.apache.spark.api.java.function.MapFunction
-import org.hl7.fhir.dstu3.model.{Bundle, Condition}
+import org.hl7.fhir.dstu3.model.{Condition}
 
+abstract class BundleMapFunction extends MapFunction[String, Condition] {
+  def call(bundleString: String): Condition
+}
 
 object BunsenTest {
-  
 
-  /** Our main function where the action happens */
+  val ctx = FhirContext.forDstu3()
+
+  val encoders = FhirEncoders.forStu3.getOrCreate
+
+  val conditionMapFunction = new BundleMapFunction {
+    override def call(bundleString: String): Condition = {
+      return ctx.newJsonParser().parseResource(bundleString).asInstanceOf[Condition]
+    }
+  }
+
   def main(args: Array[String]) {
    
     // Set the log level to only print errors
@@ -26,36 +36,24 @@ object BunsenTest {
 
     // NOTE: "multiline" option for reading multiline JSON causes
     // java.lang.IllegalAccessError: tried to access method com.google.common.base.Stopwatch.<init>()V from class org.apache.hadoop.mapreduce.lib.input.FileInputFormat
-    // => need to minify the JSONs first
 
     import spark.implicits._
 
     val bundleJsonDf = spark
       .read
       //.option("multiLine", true)
-      .text("./data/test-collection.json")
+      .text("./data/test-condition.json")
       .as[String]
 
     bundleJsonDf.show()
 
-    val ctx = FhirContext.forDstu3()
-    class BundleMapFunction extends MapFunction[String, Condition] {
-      override def call(bundleString: String): Condition = {
-        return ctx.newJsonParser().parseResource(bundleString).asInstanceOf[Condition]
-      }
-    }
-
-    val encoders = FhirEncoders.forStu3.getOrCreate
-
     val bundleDf = bundleJsonDf.map(
-      new BundleMapFunction,
+      conditionMapFunction,
       encoders.of(classOf[Condition])
     )
 
-    // Throws error: "Task not serializable: java.io.NotSerializableException"
-    // because BundleMapFunction is not serializable
     bundleDf.show()
   }
-    
+
 }
   
